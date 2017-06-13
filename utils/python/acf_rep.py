8c12605d26abe72c1b4dbbdd14ec406f342f6d6a
Hossein Tahmasbi <h.tahmasb@gmail.com>
2017-06-13 18:35:56 +0430

    minor.

--- utils/python/acf_rep.py
+++ utils/python/acf_rep.py
@@ -14,8 +14,6 @@ def replicate(na,nb,nc,atoms):
                         y = atoms.rat[i][1]+n*atoms.cellvec[0][1]+m*atoms.cellvec[1][1]+l*atoms.cellvec[2][1]
                         z = atoms.rat[i][2]+n*atoms.cellvec[0][2]+m*atoms.cellvec[1][2]+l*atoms.cellvec[2][2]
                         atoms_out.rat.append([])
-                        #atoms_out.sat.append([])
-                        #atoms_out.bemoved.append([])
                         atoms_out.rat[-1].append(x)
                         atoms_out.rat[-1].append(y)
                         atoms_out.rat[-1].append(z)
@@ -37,13 +35,13 @@ def replicate(na,nb,nc,atoms):
     atoms_out.epot=atoms.epot
     return atoms_out
 #****************************************************
-str1 = "This script replicate an acf file in the cell vector directions."
-str1+="\n The n_i are integers and b_i are reciprocal vectors. The output file is also in acf format."
+str1 = "This script replicate a structure in the cell vectors direction."
+str1+="\n The n_i are integers and a, b and c are the lattice vectors. The format of the input file must be ACF."
 parser = argparse.ArgumentParser(description=str1)
 parser.add_argument('fn_input', action='store' ,type=str, help="fn_input is the name of the input file")
-parser.add_argument('n1', type=int, help='maxmimum number of k-vectors in direction b1')
-parser.add_argument('n2', type=int, help='maxmimum number of k-vectors in direction b2')
-parser.add_argument('n3', type=int, help='maxmimum number of k-vectors in direction b3')
+parser.add_argument('n1', type=int, help='scales a by n1 factor')
+parser.add_argument('n2', type=int, help='scales b by n2 factor')
+parser.add_argument('n3', type=int, help='scales c by n3 factor')
 args=parser.parse_args()
 
 atoms_all=acf_read(args.fn_input)
