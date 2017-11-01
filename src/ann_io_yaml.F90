b29ed9757e35986a13d2d50e2275d755f7d1121b
Alireza Ghasemi <alirezagh76@gmail.com>
2017-11-01 17:09:28 +0330

    ANN TB writes and reads ANN parameters and weights in yaml format correctly.

--- src/ann_io_yaml.F90
+++ src/ann_io_yaml.F90
@@ -63,14 +63,14 @@ subroutine get_symfunc_parameters_yaml(parini,iproc,fname,ann,rcut)
     enddo
     ann%nl=ann%nl+1 !adding the output layer to total number of layers
     ann%nn(ann%nl)=1 !setting the output layer
-    if(trim(parini%subtask_ann)=='check_symmetry_function') then
-        rcut=ann%dict_ann//"main"//"rcut"
-    endif
-    if(trim(parini%approach_ann)=='atombased') then
-        rcut=ann%dict_ann//"main"//"rcut"
-    endif
+    !if(trim(parini%subtask_ann)=='check_symmetry_function') then
+    !    rcut=ann%dict_ann//"main"//"rcut"
+    !endif
+    !if(trim(parini%approach_ann)=='atombased') then
+    !    rcut=ann%dict_ann//"main"//"rcut"
+    !endif
+    rcut               =  ann%dict_ann//"main"//"rcut"
     if(trim(parini%approach_ann)=='eem1' .or. trim(parini%approach_ann)=='cent1' .or. trim(parini%approach_ann)=='cent2') then
-        rcut               =  ann%dict_ann//"main"//"rcut"
         ann%ampl_chi       =  ann%dict_ann//"main"//"ampl_chi" 
         ann%prefactor_chi  =  ann%dict_ann//"main"//"prefactor_chi" 
         ann%ener_ref       =  ann%dict_ann//"main"//"ener_ref" 
