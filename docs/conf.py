c30c46d7f27becca7d3df3da7071c89474bc6c2c
Alireza Ghasemi <alirezagh76@gmail.com>
2023-03-18 22:23:43 +0100

    Adding CI/CD to build the gitlab pages using sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -196,7 +196,7 @@ latex_elements = {
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
   ('index', 'flame.tex', u'FLAME Documentation',
-   ur'Maximilian Amsler \\ S. Alireza Ghasemi', 'manual'),
+   u'Maximilian Amsler \\\ S. Alireza Ghasemi', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
